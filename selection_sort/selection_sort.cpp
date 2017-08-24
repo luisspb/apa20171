a5ef02ec68b06c47cf6103b0ef46b4197cb64298
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2017-08-24 10:39:51 -0300

    - Add debugged Selection Sort.

--- selection_sort/selection_sort.cpp
+++ selection_sort/selection_sort.cpp
@@ -4,15 +4,19 @@
 
 void Sort::selection (std::vector<unsigned int>& v) {
    unsigned int vsize;
-   unsigned int min;
+   unsigned int min, jmin, j;
 
    vsize = v.size();
 
    for (unsigned int i = 0; i < vsize; i++) {
       min = v[i];
-      for (unsigned int j = i; j < vsize; j++)
-         if (v[j] < min)
+      jmin = i;
+      for (j = i; j < vsize; j++)
+         if (v[j] < min) {
             min = v[j];
+            jmin = j;
+         }
+      v[jmin] = v[i];
       v[i] = min;
    }
 }
