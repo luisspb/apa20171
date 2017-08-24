55a711292d1b27b2d1ad679b886742fe1d0acfc5
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2017-08-24 10:39:12 -0300

    - Add debugged Insertion Sort.

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -4,15 +4,15 @@
 
 void Sort::insertion (std::vector<unsigned int>& v) {
    unsigned int vsize;
-   unsigned int i, aux;
+   unsigned int aux;
+   int i;
 
    vsize = v.size();
 
-   for (unsigned int sorted = 0; sorted < vsize; sorted++) {
+   for (unsigned int sorted = 0; sorted < vsize-1; sorted++) {
       aux = v[sorted+1];
-      for (i = sorted; aux < v[i]; i--) {
-        v[i+1] = v[i];
-      }
+      for (i = sorted; (i >= 0) && (aux < v[i]); i--)
+         v[i+1] = v[i];
       v[i+1] = aux;
    }
 }
