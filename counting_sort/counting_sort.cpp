ebd8c496f0823e96ce4dfe442764bfbb6da55719
Jorgeluis Guerra <jorgeluis.jp@gmail.com>
2018-08-26 21:00:49 -0300

    Add procedure to find min and max values for the Counting algorithm.

--- counting_sort/counting_sort.cpp
+++ counting_sort/counting_sort.cpp
@@ -3,4 +3,18 @@
 #include "counting_sort.h"
 
 void Sort::counting (std::vector<long>& v) {
+   int min, max;
+
+   // Find the minimum and maximum values
+   min = v[0];
+   max = v[0];
+   for (unsigned i = 1; i < v.size(); i++) {
+      if (min > v[i])
+         min = v[i];
+      else if (max < v[i])
+         max = v[i];
+   }
+
+   std::cout << "\nmin = " << min << "\nmax = " << max << std::endl;
+
 }
